1026d183268a81ad952946fbf3f20d663dd379e3
Johan Thorild <johan.thorild@consid.se>
2023-02-27 12:19:44 +0100

    enable swagger on release

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -21,12 +21,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(opt =>
-    {
-        opt.SwaggerEndpoint($"juiceup/swagger.json", "JuiceUp API");
-    });
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(opt =>
+{
+    opt.SwaggerEndpoint($"juiceup/swagger.json", "JuiceUp API");
+});
 
 app.UseHttpsRedirection();
 
