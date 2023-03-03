807f857637b63f97c75bd39dab1be4075588e41b
Johan Thorild <johan.thorild@consid.se>
2023-03-03 15:33:34 +0100

    Add Restroom column for Stations

---
+++ src/Infrastructure/Migrations/20230303143312_Add Restroom column.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRestroomcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasRestroom",
+                table: "Stations",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Stations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "HasRestroom",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Stations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "HasRestroom",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Stations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "HasRestroom",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Stations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "HasRestroom",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Stations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "HasRestroom",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasRestroom",
+                table: "Stations");
+        }
+    }
+}
